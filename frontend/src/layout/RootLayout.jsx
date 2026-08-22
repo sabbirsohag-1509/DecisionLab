121600bf4bc13e8465995d6c0d25fb3269d12997
sabbirsohag-1509 <sabbirhossainsohag5@gmail.com>
2026-08-22 19:58:16 +0600

    first commit

---
+++ frontend/src/layout/RootLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router";
+import Navbar from './../component/Navbar/Navbar';
+import Footer from './../component/Footer/Footer';
+
+const RootLayout = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-base-100 text-base-content">
+      <header className="sticky top-0 z-50 bg-base-100/90 backdrop-blur-md border-b border-primary/20">
+        <Navbar />
+      </header>
+
+      <main className="flex-1 max-w-7xl mx-auto w-full px-4 lg:px-10 pb-10">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default RootLayout;
